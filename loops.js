12a815d9d7edd066c2565abd4b02b3c93d9a4fe4
Keegan Rankin <kms.rankin@gmail.com>
2018-12-27 06:38:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,9 +19,8 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   var i = 0
   function incrementVariable() {
-  i = i + 1;
-}
-  do {
+    i = i + 1;
+}   do {
     array.pop()
   } while (array.length > 0 && incrementVariable() === "true")
 }
