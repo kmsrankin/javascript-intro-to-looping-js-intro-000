d459dea20042ce5165ffb6e892418524d3d4008c
Keegan Rankin <kms.rankin@gmail.com>
2018-12-27 05:34:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,4 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
-      return array
-  } else {
-      array.push(`I am ${i} strange loops.`)
-      return array
-  }
-}
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}`)
+    return array
